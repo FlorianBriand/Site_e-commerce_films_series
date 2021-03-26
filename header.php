0fbe873ce9e0d79d4f0ceea862c3ea0bf9d8d462
FlorianBriand <briandflor@eisti.eu>
2021-03-26 21:29:43 +0100

    factorisation de code

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+echo '
+    <header>
+      <nav>
+        <img id="logo" src="img/logo.jpg" alt="" />
+        <ul>
+          <li><a href="index.html">Accueil</a></li>
+          <li><a href="films.html">Films</a></li>
+          <li><a href="series.html">Séries</a></li>
+          <li><a href="documentaire.html">Documentaires</a></li>
+          <li><a href="liste.html">Ma liste</a></li>
+          <li>
+            <input type="search" id="barsearch" placeholder="Recherche" />
+          </li>
+          <li><a href="#">S\'identifier</a></li>
+        </ul>
+      </nav>
+    </header>';
---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+<menu>
+<a href="index.html">Accueil</a> <br /><br />
+Nos produits <br /><br />
+<a href="films.html">Films</a> <br /><br />
+<a href="series.html">Séries</a> <br /><br />
+<a href="documentaire.html">Documentaires</a><br /><br />
+<a href="liste.html">Ma liste</a> <br /><br /><br />
+<a href="contact.html">Contact</a>
+</menu>';
