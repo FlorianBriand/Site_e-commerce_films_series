0b39b9243bb4aa275256b122c74e031ec1e2e72f
FlorianBriand <briandflor@eisti.eu>
2021-04-24 17:07:55 +0200

    update bdd

--- data.sql
+++ data.sql
@@ -4,7 +4,7 @@ VALUES
     ('venom.jpg', 'Venom', 'Science Fiction, Action', '10', '8', '0', '1'),
     ('farfromhome.jpg', 'Spider-man : Far From Home', 'Action, Aventure', '10', '6', '0', '1'),
     ('titanic.jpg', 'Titanic', 'Drame, Romance', '10', '8', '0', '1'),
-    ('ultron.jpg', 'Avengers : l ère d ultron', 'Action, Aventure, Science Fiction', '10', '1', '0', '1'),
+    ('ultron.jpg', 'Avengers : l ere d ultron', 'Action, Aventure, Science Fiction', '10', '1', '0', '1'),
     ('infinitywar.jpg', 'Avengers : Infinity War', 'Action, Aventure, Fantastique', '10', '1', '0', '1');
 
 INSERT INTO Series
@@ -19,14 +19,14 @@ VALUES
 INSERT INTO Documentaires
     (picture, nom, genre, prix, stock, panier, idcategorie)
 VALUES
-    ('Guerre.jpg', 'Première guerre mondiale', '', '10', '6', '0', '3'),
+    ('Guerre.jpg', 'Premiere guerre mondiale', '', '10', '6', '0', '3'),
     ('Icare.jpg', 'Icarus', '', '10', '6', '0', '3'),
     ('Mayday.jpg', 'Mayday', '', '10', '6', '0', '3'),
     ('topgear.jpg', 'TopGear', '', '10', '6', '0', '3'),
     ('Animal.jpg', 'Meilleur espoir animal', '', '10', '6', '0', '3');
 
 INSERT INTO categorie
-    (idcategorie, nom)
+    (id, nom)
 VALUES
     ('1', 'Films'),
     ('2', 'Series'),
