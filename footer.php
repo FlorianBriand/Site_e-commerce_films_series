ba150517544598d160b8da35e67b133661c30c80
FlorianBriand <briandflor@eisti.eu>
2021-04-25 21:54:40 +0200

    ajout nous contacter au footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="w-full text-white text-center mt-20 pb-20">
       <br> Copyright Société Netchill<br />Raphael Coutinho et Florian Briand<br />Contact
-      us :<br />netchill@gmail.com<br />06.01.23.45.67 <br />Avenue du Parc, 95000 Cergy
+      us :<br />netchill@gmail.com<br />06.01.23.45.67 <br />Avenue du Parc, 95000 Cergy<br><a class="hover:underline my-5 " href="contact.php">Nous contacter</a>
 
     </footer>
